4bb34b6c151d97285ef2ff1d1e6144824e9e511e
naka <nakajima@jcsc.co.jp>
2017-07-02 00:50:58 +0900

    mock -> view

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -66,7 +66,8 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::loadAll(); // Loads all plugins at once
  * CakePlugin::load('DebugKit'); // Loads a single plugin named DebugKit
  */
-
+CakePlugin::loadAll(); // Loads all plugins at once
+CakePlugin::load('DebugKit'); // Loads a single plugin named DebugKit
 /**
  * To prefer app translation over plugin translation, you can set
  *
