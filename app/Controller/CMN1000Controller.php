ea5b5927353bd18139de3f087a020d96ee15141a
藤原幸史 <fujiwara_k@jcsc.co.jp>
2017-07-04 23:28:58 +0900

    ログイン連続3回失敗で1分間ロックするようにした

--- app/Controller/CMN1000Controller.php
+++ app/Controller/CMN1000Controller.php
@@ -6,10 +6,12 @@ class CMN1000Controller extends AppController {
 	public $helpers = ['Html', 'Form'];
 	
 	// 使用するモデル
-	public $uses = ['Notification', 'User'];
+	public $uses = ['Notification', 'User', 'InvalidAccess'];
 	
 	public function index() {
 		$this->set("title_for_layout","ログイン"); 
+		
+		$this->set('failures', $this->InvalidAccess->validate($this->request->clientIp(false)));
 		$this->set('notifications', $this->Notification->getNotification());
 	}
 	
@@ -18,8 +20,10 @@ class CMN1000Controller extends AppController {
 			//$this->log($this->request, LOG_DEBUG);
 			$user = $this->User->login($this->request->data('txtLoginId'), $this->request->data('txtPassword'));
 			if(empty($user)){
+				$this->InvalidAccess->save(['InvalidAccess' => ['CLIENT_IP' => $this->request->clientIp(false), 'ACCESS_DATETIME' => DboSource::expression('NOW()')] ]);
 				$this->setError('ログインできません。ユーザＩＤ、パスワードを確認してください。(ERR_CMN1000_01)');
 			} else {
+				$this->InvalidAccess->deleteAll(['InvalidAccess.CLIENT_IP' => $this->request->clientIp(false)], false);
 				// セッションを開始して次の画面に遷移する
 				if(!isset($_SESSION)) {
 					session_start();
