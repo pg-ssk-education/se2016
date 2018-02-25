5435dc76eb4d34aa8704dbac75b4d994de5c13a2
noriyuki-sasaki <mail@pg-ssk.com>
2018-02-26 02:51:21 +0900

    作成中

---
+++ NoDeploy/e2e/sample.php
@@ -0,0 +1,32 @@
+<?php
+class WebDriverTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->setHost("localhost");
+        $this->setPort(4444);
+        $this->setBrowser('chrome');
+        $this->setBrowserUrl('http://localhost/se2016/');
+    }
+    
+    public function test_google()
+    {
+        $this->url('http://localhost/se2016/');
+        $this->assertStringStartsWith('ログイン:', $this->title());
+                
+        $input = $this->byId('txtLoginId');
+        $input->value('admin');
+
+        $this->byId('txtPassword')->value("admin");
+        
+        $this->submit();
+        
+//        $this->byId('btnLogin').click();
+        $this->assertStringStartsWith('トップ:', $this->title());
+    
+        $fp = fopen('hoge.png', 'w');
+        fwrite($fp, $this->currentScreenshot());
+        fclose($fp);
+    }
+}
