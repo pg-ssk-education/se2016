ad9d3243308c3d9d1b63ff4b750edc423590e01d
200825 <s-ueda@jcsc.co.jp>
2018-07-16 17:53:09 +0900

    defaultデータ修正

--- NoDeploy/data/default.sql
+++ NoDeploy/data/default.sql
@@ -3,31 +3,24 @@ INSERT INTO se2016_M_USER (
   USER_ID,
   PASSWORD,
   NAME,
-  NAME_KANA,
-  PASSWORD_LIMIT,
-  PASSWORD_KEY
+  NAME_KANA
 ) VALUES (
   'admin',
   '0fdbe4a8deee26297be7b23a21c742880af452fec8f0a1d9ac9e9aa4a2a511ce',
   '管理者',
-  'カンリシャ',
-  '2000-01-01',
-  ''
+  'カンリシャ'
 );
 
 DELETE FROM se2016_T_NOTIFICATION;
 INSERT INTO se2016_T_NOTIFICATION (
-  NOTIFICATION_ID,
   TARGET_USER_ID,
   LEVEL,
   COMMENT
 ) VALUES (
-  '1',
   '',
   'I',
   '公開情報'
 ),(
-  '2',
   'admin',
   'I',
   'adminに対する情報'
